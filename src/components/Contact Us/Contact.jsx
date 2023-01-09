1519ab9c92914e9a3f246ea4dd9455e80f081d07
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-01-09 20:27:23 +0600

    add contact info

--- src/components/Contact Us/Contact.jsx
+++ src/components/Contact Us/Contact.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import PageHeader from '../About/AboutSections/PageHeader';
+import SectionHeading from './../Shared/SectionHeading';
+import ContactInfo from './ContactSections/ContactInfo';
 
 
 const Contact = () => {
-
+    const heading = {
+        primary: "HOW TO FIND US", secondary: "We are Here to Help You"
+    }
 
 
     return (
         <div>
             <PageHeader title="contact us" ></PageHeader>
+            <div className='mt-10'></div>
+            <SectionHeading heading={heading}></SectionHeading>
+            <ContactInfo />
         </div>
     );
 };

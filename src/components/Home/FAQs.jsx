3984b3a773ffb9d15f24aaba2d8ba62b1f59591c
ismailjosim <ismailjosim99@gmail.com>
2023-02-10 22:37:38 +0600

    change logo and change FAQs

--- src/components/Home/FAQs.jsx
+++ src/components/Home/FAQs.jsx
@@ -13,133 +13,117 @@ export const FAQs = () => {
                     <div className="py-5">
                         <details className="group">
                             <summary className="flex justify-between items-center font-medium cursor-pointer list-none">
-                                <span className='text-semibold lg:text-xl text-base'> What is a SAAS platform?</span>
+                                <span className='text-semibold lg:text-xl text-base'>What are your office hours?</span>
                                 <span className="transition group-open:rotate-45">
                                     <IoIosAddCircle className="transition group-open:rotate-45 bg-primary rounded-full text-white text-2xl" />
                                 </span>
                             </summary>
                             <p className="text-neutral-600 mt-3 group-open:animate-fadeIn">
-                                SAAS platform is a cloud-based software service that allows users to access
-                                and use a variety of tools and functionality.
+                                Our office is open from 9:00 AM to 5:00 PM from Monday to Friday, and from 9:00 AM to 2:00 PM on Saturdays.
                             </p>
                         </details>
                     </div>
                     <div className="py-5">
                         <details className="group">
                             <summary className="flex justify-between items-center font-medium cursor-pointer list-none">
-                                <span className='text-semibold lg:text-xl text-base'> How does  billing work?</span>
+                                <span className='text-semibold lg:text-xl text-base'>What services do you offer?</span>
                                 <span className="transition group-open:rotate-45">
                                     <IoIosAddCircle className="transition group-open:rotate-45 bg-primary rounded-full text-white text-2xl" />
                                 </span>
                             </summary>
                             <p className="text-neutral-600 mt-3 group-open:animate-fadeIn">
-                                We offers a variety of billing options, including monthly and annual subscription plans,
-                                as well as pay-as-you-go pricing for certain services. Payment is typically made through a credit
-                                card or other secure online payment method.
+                                We offer a full range of dental services, including regular check-ups and cleanings, fillings, root canal therapy, crowns and bridges, dental implants, and cosmetic dentistry.
                             </p>
                         </details>
                     </div>
                     <div className="py-5">
                         <details className="group">
                             <summary className="flex justify-between items-center font-medium cursor-pointer list-none">
-                                <span className='text-semibold lg:text-xl text-base'> Can I get a refund for my subscription?</span>
+                                <span className='text-semibold lg:text-xl text-base'>Do you accept insurance?</span>
                                 <span className="transition group-open:rotate-45">
                                     <IoIosAddCircle className="transition group-open:rotate-45 bg-primary rounded-full text-white text-2xl" />
                                 </span>
                             </summary>
                             <p className="text-neutral-600 mt-3 group-open:animate-fadeIn">
-                                We offers a 30-day money-back guarantee for most of its subscription plans. If you are not
-                                satisfied with your subscription within the first 30 days, you can request a full refund. Refunds
-                                for subscriptions that have been active for longer than 30 days may be considered on a case-by-case
-                                basis.
+                                Yes, we accept most major insurance plans and will be happy to file claims on your behalf.
                             </p>
                         </details>
                     </div>
                     <div className="py-5">
                         <details className="group">
                             <summary className="flex justify-between items-center font-medium cursor-pointer list-none">
-                                <span className='text-semibold lg:text-xl text-base'> How do I cancel my subscription?</span>
+                                <span className='text-semibold lg:text-xl text-base'>What should I do if I have a dental emergency</span>
                                 <span className="transition group-open:rotate-45">
                                     <IoIosAddCircle className="transition group-open:rotate-45 bg-primary rounded-full text-white text-2xl" />
                                 </span>
                             </summary>
                             <p className="text-neutral-600 mt-3 group-open:animate-fadeIn">
-                                To cancel your We subscription, you can log in to your account and navigate to the
-                                subscription management page. From there, you should be able to cancel your subscription and stop
-                                future billing.
+                                If you have a dental emergency during office hours, please call us immediately and we will do our best to see you as soon as possible. If you have an emergency after hours, please follow the instructions provided on our answering machine for emergency contact information.
                             </p>
                         </details>
                     </div>
                     <div className="py-5">
                         <details className="group">
                             <summary className="flex justify-between items-center font-medium cursor-pointer list-none">
-                                <span className='text-semibold lg:text-xl text-base'> Can I try this platform for free?</span>
+                                <span className='text-semibold lg:text-xl text-base'>How often should I visit the dentist for a check-up and cleaning?</span>
                                 <span className="transition group-open:rotate-45">
                                     <IoIosAddCircle className="transition group-open:rotate-45 bg-primary rounded-full text-white text-2xl" />
                                 </span>
                             </summary>
                             <p className="text-neutral-600 mt-3 group-open:animate-fadeIn">
-                                We offers a free trial of its  platform for a limited time. During the trial period,
-                                you will have access to a limited set of features and functionality, but you will not be charged.
+                                Most dental professionals recommend visiting the dentist every six months for a regular check-up and cleaning. However, the frequency of your visits may vary depending on your individual needs.
                             </p>
                         </details>
                     </div>
                     <div className="py-5">
                         <details className="group">
                             <summary className="flex justify-between items-center font-medium cursor-pointer list-none">
-                                <span className='text-semibold lg:text-xl text-base'> How do I access   documentation?</span>
+                                <span className='text-semibold lg:text-xl text-base'>What is the cost of your services?</span>
                                 <span className="transition group-open:rotate-45">
                                     <IoIosAddCircle className="transition group-open:rotate-45 bg-primary rounded-full text-white text-2xl" />
                                 </span>
                             </summary>
                             <p className="text-neutral-600 mt-3 group-open:animate-fadeIn">
-                                Documentation is available on the company's website and can be accessed by
-                                logging in to your account. The documentation provides detailed information on how to use the ,
-                                as well as code examples and other resources.
+                                The cost of our services varies depending on the type of treatment you need. We will provide you with a detailed estimate of the cost of your treatment after a thorough examination and discussion of your options.
                             </p>
                         </details>
                     </div>
                     <div className="py-5">
                         <details className="group">
                             <summary className="flex justify-between items-center font-medium cursor-pointer list-none">
-                                <span className='text-semibold lg:text-xl text-base'> How do I contact support?</span>
+                                <span className='text-semibold lg:text-xl text-base'>Do you use sedation dentistry for anxious patients?</span>
                                 <span className="transition group-open:rotate-45">
                                     <IoIosAddCircle className="transition group-open:rotate-45 bg-primary rounded-full text-white text-2xl" />
                                 </span>
                             </summary>
                             <p className="text-neutral-600 mt-3 group-open:animate-fadeIn">
-                                If you need help with the platform or have any other questions, you can contact the
-                                company's support team by submitting a support request through the website or by emailing
-                                support@We.com.
+                                Yes, we offer several options for sedation dentistry to help our patients feel comfortable and relaxed during their appointments.
                             </p>
                         </details>
                     </div>
                     <div className="py-5">
                         <details className="group">
                             <summary className="flex justify-between items-center font-medium cursor-pointer list-none">
-                                <span className='text-semibold lg:text-xl text-base'> Do you offer any discounts or promotions?</span>
+                                <span className='text-semibold lg:text-xl text-base'>How long does it take to get a dental implant?</span>
                                 <span className="transition group-open:rotate-45">
                                     <IoIosAddCircle className="transition group-open:rotate-45 bg-primary rounded-full text-white text-2xl" />
                                 </span>
                             </summary>
                             <p className="text-neutral-600 mt-3 group-open:animate-fadeIn">
-                                We may offer discounts or promotions from time to time. To stay up-to-date on the latest
-                                deals and special offers, you can sign up for the company's newsletter or follow it on social media.
+                                The length of time it takes to complete a dental implant procedure can vary, but it typically takes several months from start to finish.
                             </p>
                         </details>
                     </div>
                     <div className="py-5">
                         <details className="group">
                             <summary className="flex justify-between items-center font-medium cursor-pointer list-none">
-                                <span className='text-semibold lg:text-xl text-base'> How do we compare to other similar services?</span>
+                                <span className='text-semibold lg:text-xl text-base'>How can I make an appointment?</span>
                                 <span className="transition group-open:rotate-45">
                                     <IoIosAddCircle className="transition group-open:rotate-45 bg-primary rounded-full text-white text-2xl" />
                                 </span>
                             </summary>
                             <p className="text-neutral-600 mt-3 group-open:animate-fadeIn">
-                                This platform is a highly reliable and feature-rich service that offers a wide range
-                                of tools and functionality. It is competitively priced and offers a variety of billing options to
-                                suit different needs and budgets.
+                                You can make an appointment by calling our office or by using our online appointment request form.
                             </p>
                         </details>
                     </div>
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../Contexts/AuthProvider';
-import logo from '../assets/images/dental-final-logo.png';
+import logo from '../assets/images/logo.png';
 
 const Header = () => {
     const { user, userLogout } = useContext(AuthContext);
@@ -16,11 +16,11 @@ const Header = () => {
         <li><Link className='hover:btn-secondary rounded-md hover:text-white' to='/about'>About US</Link></li>
         <li><Link className='hover:btn-secondary rounded-md hover:text-white' to='/appointment'>Appointment</Link></li>
         <li><Link className='hover:btn-secondary rounded-md hover:text-white' to='/contact'>Contact Us</Link></li>
-        {user?.uid ?
+        { user?.uid ?
             <>
-                <li><p className='uppercase'>{user.displayName}</p></li>
+                <li><p className='uppercase'>{ user.displayName }</p></li>
                 <li><Link className='hover:btn-secondary rounded-md hover:text-white' to='/dashboard'>Dashboard</Link></li>
-                <li><button onClick={handleUserLogout} className='hover:btn-secondary btn-primary text-white rounded-md'>Logout</button></li>
+                <li><button onClick={ handleUserLogout } className='hover:btn-secondary btn-primary text-white rounded-md'>Logout</button></li>
             </> :
             <li><Link className='hover:btn-secondary rounded-md hover:text-white' to='/login'>Login</Link></li>
         }
@@ -32,30 +32,30 @@ const Header = () => {
     return (
         <header className='border-b-2'>
             <div className="navbar font-semibold lg:w-11/12 mx-auto">
-                <label htmlFor="dashboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
+                <label htmlFor="dashboard-drawer" tabIndex={ 2 } className="btn btn-ghost lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                 </label>
                 <div className="navbar-start">
-                    <Link className='' to='/'>
-                        <img className='w-48' src={logo} alt="" />
+                    <Link to='/'>
+                        <img src={ logo } alt="Dental" />
                     </Link>
                 </div>
                 <div className="navbar-end">
                     <div className="navbar-center hidden lg:flex">
                         <ul className="menu menu-horizontal p-0 gap-3">
-                            {menuItems}
+                            { menuItems }
                         </ul>
                     </div>
                     <div className="dropdown dropdown-bottom dropdown-end">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <label tabIndex={ 0 } className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                             </svg>
                         </label>
                         <ul className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            {menuItems}
+                            { menuItems }
                         </ul>
                     </div>
 
